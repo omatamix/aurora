b26e980ac84dd2a705b0f845c5fa9cefe9dc83d1
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-13 01:20:21 -0400

    Create RequestHandler.php

---
+++ src/RequestHandler.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Omatamix\RequestLock;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Manage HTTP/RestfulAPI requests.
+ */
+final class RequestHandler implements RequestHandlerInterface
+{
+}
